fbb1bd51cd7923b1703bde357e6d013233e5aaa4
jangle <shubhamjangale0000@gmail.com>
2020-09-27 21:07:26 +0530

    UC2 commited

--- SnakeLadderGame.java
+++ SnakeLadderGame.java
@@ -1,6 +1,9 @@
 public class SnakeLadderGame{
 	public static void main(String[] args) {
 		int start = 0;
+		int start_check = (int)Math.floor(Math.random()*10)%6;
+		
 		System.out.println("Player1 starts at " + start);
+		System.out.println("Player1 starts by rolling " + start_check);
 	}
 }
