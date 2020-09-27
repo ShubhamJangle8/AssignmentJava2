dacb9d88ed9faaabe40fb55ddb4a9fd3e5fab413
jangle <shubhamjangale0000@gmail.com>
2020-09-27 21:12:58 +0530

    UC3 commited

--- SnakeLadderGame.java
+++ SnakeLadderGame.java
@@ -1,9 +1,25 @@
-public class SnakeLadderGame{
+public class SnakeLadderGame {
 	public static void main(String[] args) {
-		int start = 0;
-		int start_check = (int)Math.floor(Math.random()*10)%6;
+		//constants
+		final int no_play = 0;
+		final int snake = 1;
+		final int ladder = 2;
+		 
+		//variables
+		int player1_position = 0;
 		
-		System.out.println("Player1 starts at " + start);
-		System.out.println("Player1 starts by rolling " + start_check);
+		//throwing a dice
+		int dice_num = (int)Math.floor(Math.random() * 10) % 6 + 1 ;
+
+		System.out.println("Number on dice : "+dice_num);
+
+		int option_check = (int)Math.floor(Math.random() * 10) % 3;
+
+		if(option_check == no_play)
+			System.out.println("On a same position");
+		else if(option_check == ladder)
+			System.out.println("Move ahead by "+dice_num+" positions");
+		else
+			System.out.println("Move behind by "+dice_num+" positions");
 	}
 }
